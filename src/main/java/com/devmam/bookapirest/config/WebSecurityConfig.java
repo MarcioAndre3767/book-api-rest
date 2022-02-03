fff4975c7a2ba9b626949b83ac17e5e5751f7aa6
MarcioAndre3767 <marcioandre.ti1@gmail.com>
2022-02-03 04:53:44 -0300

    Created client

--- src/main/java/com/devmam/bookapirest/config/WebSecurityConfig.java
+++ src/main/java/com/devmam/bookapirest/config/WebSecurityConfig.java
@@ -2,6 +2,7 @@
 package com.devmam.bookapirest.config;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
@@ -24,6 +25,7 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 	protected void configure(HttpSecurity http) throws Exception {
 		http.authorizeRequests()
 			.antMatchers("/h2-console/**").permitAll()
+			.antMatchers(HttpMethod.OPTIONS, "/**").permitAll()
 			.anyRequest().authenticated()			
 				.and()
 					.httpBasic()			//autenticação básica
